ab033a9e0c86735ff9bf780bd5fa6f03f2ed142b
erak <erik@zirkular.io>
2021-02-04 22:44:58 +0100

    Adjusts video paths in 3D pose estimation to new motion database.

--- detector/predict_3d_pose.py
+++ detector/predict_3d_pose.py
@@ -24,14 +24,14 @@ def get_parser():
     return parser
 
 
-def run_external_script(folder):
+def run_external_script(basedir, folder):
     print("----------------------------------------------------------")
     print("to-share::detector")
     print("----------------------------------------------------------")
     print(subprocess.check_output([
         'python', 'run.py',
         '-d', 'custom',
-        '-k', '../../data/train/' + folder + '/pose2d',
+        '-k', basedir + '/' + folder + '/pose2d',
         '-arc', '3,3,3,3,3',
         '-c', 'checkpoint',
         '--evaluate', 'pretrained_243_h36m_detectron_coco_wtraj.bin',
@@ -39,28 +39,28 @@ def run_external_script(folder):
         '--viz-subject', 'detectron2',
         '--viz-action', 'custom',
         '--viz-camera', '0',
-        '--viz-video', '../../data/train/' + folder + '/input_seq.mp4',
-        '--viz-output', '../../data/train/' + folder + '/pose3d.mp4',
-        '--viz-export', '../../data/train/' + folder + '/pose3d',
+        '--viz-video', basedir + '/' + folder + '/input.mp4',
+        '--viz-output', basedir + '/' + folder + '/pose3d.mp4',
+        '--viz-export', basedir + '/' + folder + '/pose3d',
         '--viz-size', '6']))
 
 if __name__ == '__main__':
     args = get_parser().parse_args()
 
     os.chdir('VideoPose3D')
-    basedir = '../../data/train/'
+    basedir = '../../data/train/video/deep-dance'
     for video_folder in os.listdir(basedir):
-        for folder in os.listdir(basedir + video_folder):
-            pose3d_video = basedir + video_folder +'/' + folder + '/pose3d.npz'
+        for folder in os.listdir(basedir + '/' + video_folder):
+            pose3d_video = basedir  + '/' + video_folder + '/' + folder + '/pose3d.npz'
             print(pose3d_video)
             if path.isfile(pose3d_video):
                 print("3D pose estimation found.")
                 if args.overwrite:
                     print("Flag detected. Overwriting...")
-                    run_external_script(video_folder +'/' + folder)
+                    run_external_script(basedir, video_folder + '/' + folder)
                 else:
                     print("Skipping.")
             else:
-                run_external_script(video_folder +'/' + folder)
+                run_external_script(basedir, video_folder + '/' + folder)
 
     os.chdir('..')
