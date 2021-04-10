3dacd344f7ed4efd0393b4ace1286ec700510c35
Erik K <erik@zirkular.io>
2021-04-10 15:36:25 +0000

    dvc: commit experiment 49fcabfe432316433f838584a29e2ccc6bad429c3dbc4300052109de4ad38ffe

--- creator/exp_run.sh
+++ creator/exp_run.sh
@@ -32,6 +32,7 @@ dvc exp run --queue -S train.epochs=10 -S train.batch_size=32 -S train.look_back
 dvc exp run --queue -S train.epochs=10 -S train.batch_size=32 -S train.look_back=10 -S train.lstm_layer=128 -S train.mdn_layer=5 -S train.dancers=all -S generate.look_back=10 -S generate.dancers=all
 dvc exp run --run-all --jobs 1
 
+
 # Run queued experiments
 # ---------------------------
 # Or run in parallel
